f9735b55993deac67d99eebec6d7a1334a93b42f
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-28 22:04:41 -0300

    :sparkles: add show action

--- lib/hello_phoenix_web/controllers/hello_controller.ex
+++ lib/hello_phoenix_web/controllers/hello_controller.ex
@@ -4,4 +4,8 @@ defmodule HelloPhoenixWeb.HelloController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def show(conn, %{"person" => name}) do
+    render(conn, "show.html", person_name: name)
+  end
 end
